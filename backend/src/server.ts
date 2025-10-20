cd43017dca5ee1cd2690fa3e6f8d26b7f018e233
fmcubium <andresdelaflorad@gmail.com>
2025-10-20 17:54:38 -0400

    Added some routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,4 @@
-const express = require('express');
+import express, { Request, Response } from 'express';
 const mongoose = require('mongoose');
 require('dotenv').config();
 
@@ -14,3 +14,39 @@ app.listen(PORT, () => {
  mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => console.log('MongoDB connected successfully'))
         .catch((err: any) => console.error('MongoDB connection error:', err));
+const db = mongoose.connection.Db;
+
+// Begin API Requests
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello, world!");
+});
+
+app.post("/api/create", (req: Request, res: Response) => {
+    // change to accept auth things later
+
+
+});
+
+app.get("/api/user", (req: Request, res: Response) => {
+    const username = req.query.username;
+    const user = db.users.find({ "username": username });
+    res.send(user);
+});
+
+// app.get("/api/goals", (req: Request, res: Response) => {
+//     const username = req.query.username;
+//     const goals = db.users.find({ "username": username }, { "goals": 1, "_id": 0 });
+//     res.send(goals);
+// });
+
+// app.get("/api/budget", (req: Request, res: Response) => {
+//     const username = req.query.username;
+//     const budget = db.users.find({ "username": username }, { "budget": 1, "_id": 0 });
+//     res.send(budget);
+// });
+
+// app.get("/api/categories", (req: Request, res: Response) => {
+//     const username = req.query.username;
+//     const budget = db.users.find({ "username": username }, { "categories": ""})
+// })
